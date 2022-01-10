4c74697a3639a9c4347fcf6648e557bd40d1a8a5
Tharlei Aleixo <tharlei_aleixo@hotmail.com>
2022-01-10 00:11:35 -0300

    Adicionar sku na listagem movimentação de estoque

--- app/Modules/ProductStockMovement/Queries/ListStockMovement/StockMovementData.php
+++ app/Modules/ProductStockMovement/Queries/ListStockMovement/StockMovementData.php
@@ -4,15 +4,18 @@ namespace App\Modules\ProductStockMovement\Queries\ListStockMovement;
 
 class StockMovementData
 {
+    public string $sku;
     public int $amount;
     public string $operation;
     public string $createdAt;
 
     public function __construct(
+        string $sku,
         int $amount,
         string $operation,
         string $createdAt
     ) {
+        $this->sku = $sku;
         $this->amount = $amount;
         $this->operation = $operation;
         $this->createdAt = $createdAt;
